03fbd3f3be67956a327b6f55f5c9c8d8e263079a
VisAdevil <47916864+VisAdevil@users.noreply.github.com>
2019-12-01 17:20:28 +0100

    1200 --> 4000 carousel effect

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,7 +23,7 @@ $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in desi
 var $anchor = $(this);
 $('html, body').stop().animate({
 scrollTop: $($anchor.attr('href')).offset().top
-}, 1200, 'easeInOutExpo');
+}, 4000, 'easeInOutExpo');
 event.preventDefault();
 });
 
